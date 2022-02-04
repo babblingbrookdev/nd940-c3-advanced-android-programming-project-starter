a692a5db5a76147d0641566633333fb915dba3b2
JLRoberts <babblingbrookdev@gmail.com>
2022-02-03 23:16:44 -0500

    refactor LoadingButton
    Load when a selection has been made, otherwise show snackbar for user to make a selection.
    
    cleanup code in loading button.

--- starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -8,7 +8,6 @@ import android.graphics.Paint
 import android.graphics.Typeface
 import android.util.AttributeSet
 import android.view.View
-import androidx.core.content.ContextCompat
 import androidx.core.content.withStyledAttributes
 import kotlin.properties.Delegates
 
@@ -20,19 +19,16 @@ class LoadingButton @JvmOverloads constructor(
 ) : View(context, attrs, defStyleAttr) {
     private var widthSize = 0
     private var heightSize = 0
-
     private var buttonProgress = 0f
-    private val arcMarginRight = 50f
-    private val arcRadius = 40f
 
-    private var buttonTextSize = resources.getDimension(R.dimen.default_button_text_size)
-    private var buttonTextColor = ContextCompat.getColor(context, R.color.default_button_text_color)
-    private var buttonBackgroundColor =
-        ContextCompat.getColor(context, R.color.default_button_background_color)
-    private var buttonCircleColor =
-        ContextCompat.getColor(context, R.color.default_button_circle_color)
-    private var buttonProgressColor =
-        ContextCompat.getColor(context, R.color.default_button_progress_color)
+    private var buttonArcMargin = 50f
+    private var buttonArcRadius = 40f
+
+    private var buttonTextSize = context.resources.getDimension(R.dimen.default_button_text_size)
+    private var buttonTextColor = context.getColor(R.color.default_button_text_color)
+    private var buttonBackgroundColor = context.getColor(R.color.default_button_background_color)
+    private var buttonCircleColor = context.getColor(R.color.default_button_circle_color)
+    private var buttonProgressColor = context.getColor(R.color.default_button_progress_color)
 
     private val valueAnimator = ValueAnimator()
 
@@ -44,44 +40,41 @@ class LoadingButton @JvmOverloads constructor(
         typeface = Typeface.create("", Typeface.BOLD)
     }
 
-    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
+    private var buttonState: ButtonState by Delegates.observable(ButtonState.Completed) { _, old, new ->
+        if (old == new) return@observable
         when (new) {
             ButtonState.Clicked -> {
                 buttonProgress = 0f
                 valueAnimator.cancel()
             }
             ButtonState.Loading -> {
-                valueAnimator.apply {
-                    removeAllUpdateListeners()
-                    setValues(PropertyValuesHolder.ofFloat(PROGRESS, 0f, 1f))
-                    duration = ANIMATION_DURATION
-                    repeatCount = ValueAnimator.INFINITE
-                    repeatMode = ValueAnimator.RESTART
-                    addUpdateListener {
-                        buttonProgress = it.getAnimatedValue(PROGRESS) as Float
-                        invalidate()
-                    }
-                }.start()
+                startAnimation()
             }
             ButtonState.Completed -> {
                 buttonProgress = 1f
                 valueAnimator.cancel()
             }
         }
-        invalidate()
     }
 
 
     init {
-        context.withStyledAttributes(attrs, R.styleable.LoadingButton) {
-            buttonBackgroundColor =
-                getColor(R.styleable.LoadingButton_buttonBackgroundColor, buttonBackgroundColor)
-            buttonProgressColor =
-                getColor(R.styleable.LoadingButton_buttonProgressColor, buttonProgressColor)
-            buttonCircleColor =
-                getColor(R.styleable.LoadingButton_buttonCircleColor, buttonCircleColor)
-            buttonTextColor = getColor(R.styleable.LoadingButton_buttonTextColor, buttonTextColor)
-            buttonTextSize = getFloat(R.styleable.LoadingButton_buttonTextSize, buttonTextSize)
+        // from https://developer.android.com/training/custom-views/create-view
+        // typed array objects are a shared resource, so recycle
+        context.theme.obtainStyledAttributes(attrs, R.styleable.LoadingButton, 0, 0).apply {
+            try {
+                buttonBackgroundColor =
+                    getColor(R.styleable.LoadingButton_buttonBackgroundColor, buttonBackgroundColor)
+                buttonProgressColor =
+                    getColor(R.styleable.LoadingButton_buttonProgressColor, buttonProgressColor)
+                buttonCircleColor =
+                    getColor(R.styleable.LoadingButton_buttonCircleColor, buttonCircleColor)
+                buttonTextColor =
+                    getColor(R.styleable.LoadingButton_buttonTextColor, buttonTextColor)
+                buttonTextSize = getFloat(R.styleable.LoadingButton_buttonTextSize, buttonTextSize)
+            } finally {
+                recycle()
+            }
         }
         isClickable = true
     }
@@ -89,46 +82,16 @@ class LoadingButton @JvmOverloads constructor(
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        if (canvas == null) return
 
-        if (canvas == null) {
-            return
-        }
-
-        paint.color = buttonBackgroundColor
-        canvas.drawRect(0f, 0f, width.toFloat(), height.toFloat(), paint)
+        drawInitialBackground(canvas)
 
         if (buttonState == ButtonState.Loading) {
-
-            paint.color = buttonProgressColor
-            canvas.drawRect(0f, 0f, buttonProgress * width, height.toFloat(), paint)
-
-            paint.color = buttonTextColor
-            canvas.drawText(
-                resources.getString(R.string.button_loading),
-                width.toFloat() / 2,
-                (height.toFloat() / 2) - (paint.descent() + paint.ascent() / 2),
-                paint
-            )
-
-            paint.color = buttonCircleColor
-            canvas.drawArc(
-                width.toFloat() - arcMarginRight - (arcRadius * 2),
-                height.toFloat() / 2f - arcRadius,
-                width.toFloat() - arcMarginRight,
-                height.toFloat() / 2f + arcRadius,
-                270f,
-                360f * buttonProgress,
-                true,
-                paint
-            )
+            drawButtonProgress(canvas)
+            drawButtonLoadingText(canvas)
+            drawButtonCircle(canvas)
         } else {
-            paint.color = buttonTextColor
-            canvas.drawText(
-                resources.getString(R.string.button_name),
-                width.toFloat() / 2,
-                (height.toFloat() / 2) - (paint.descent() + paint.ascent() / 2),
-                paint
-            )
+            drawInitialText(canvas)
         }
     }
 
@@ -145,8 +108,71 @@ class LoadingButton @JvmOverloads constructor(
         setMeasuredDimension(w, h)
     }
 
-    override fun performClick(): Boolean {
+    private fun startAnimation() {
+        valueAnimator.apply {
+            removeAllUpdateListeners()
+            setValues(PropertyValuesHolder.ofFloat(PROGRESS, 0f, 1f))
+            duration = ANIMATION_DURATION
+            repeatCount = ValueAnimator.INFINITE
+            repeatMode = ValueAnimator.RESTART
+            addUpdateListener {
+                buttonProgress = it.getAnimatedValue(PROGRESS) as Float
+                invalidate()
+            }
+        }.start()
+    }
+
+    private fun drawInitialBackground(canvas: Canvas) {
+        paint.color = buttonBackgroundColor
+        canvas.drawRect(0f, 0f, width.toFloat(), height.toFloat(), paint)
+    }
+
+    private fun drawInitialText(canvas: Canvas) {
+        paint.color = buttonTextColor
+        canvas.drawText(
+            resources.getString(R.string.button_name),
+            width.toFloat() / 2,
+            (height.toFloat() / 2) - (paint.descent() + paint.ascent() / 2),
+            paint
+        )
+    }
+
+    private fun drawButtonProgress(canvas: Canvas) {
+        paint.color = buttonProgressColor
+        canvas.drawRect(0f, 0f, buttonProgress * width, height.toFloat(), paint)
+    }
+
+    private fun drawButtonLoadingText(canvas: Canvas) {
+        paint.color = buttonTextColor
+        canvas.drawText(
+            resources.getString(R.string.button_loading),
+            width.toFloat() / 2,
+            (height.toFloat() / 2) - (paint.descent() + paint.ascent() / 2),
+            paint
+        )
+    }
+
+    private fun drawButtonCircle(canvas: Canvas) {
+        paint.color = buttonCircleColor
+        canvas.drawArc(
+            width.toFloat() - buttonArcMargin - (buttonArcRadius * 2),
+            height.toFloat() / 2f - buttonArcRadius,
+            width.toFloat() - buttonArcMargin,
+            height.toFloat() / 2f + buttonArcRadius,
+            270f,
+            360f * buttonProgress,
+            true,
+            paint
+        )
+    }
+
+    fun onStartDownload() {
+        isEnabled = false
         buttonState = ButtonState.Loading
-        return super.performClick()
+    }
+
+    fun onDownloadCompleted() {
+        isEnabled = true
+        buttonState = ButtonState.Completed
     }
 }
--- starter/app/src/main/java/com/udacity/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/MainActivity.kt
@@ -11,33 +11,50 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.snackbar.Snackbar
 import com.udacity.databinding.ActivityMainBinding
-import com.udacity.databinding.ContentMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
     private var downloadID: Long = 0
+    private var url = ""
 
+    private lateinit var viewModel: MainViewModel
     private lateinit var binding: ActivityMainBinding
-    private lateinit var contentBinding: ContentMainBinding
 
     private lateinit var notificationManager: NotificationManager
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
 
+    private var itemSelected = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        contentBinding = ContentMainBinding.inflate(layoutInflater)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         val view = binding.root
         setContentView(view)
         setSupportActionBar(binding.toolbar)
 
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
-        contentBinding.customButton.setOnClickListener {
-            download()
+        binding.customButton.setOnClickListener {
+            if (itemSelected) {
+                binding.customButton.onStartDownload()
+            } else {
+                showSnackbar("Please make a selection")
+            }
+        }
+
+        binding.selectionRadioGroup.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.glide_radio_selection -> url = GLIDE_URL
+                R.id.loadapp_radio_selection -> url = LOADAPP_URL
+                R.id.retrofit_radio_selection -> url = RETROFIT_URL
+            }
+            itemSelected = true
         }
     }
 
@@ -47,9 +64,13 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun download() {
+    private fun showSnackbar(message: String) {
+        Snackbar.make(binding.root, message, Snackbar.LENGTH_SHORT).show()
+    }
+
+    private fun download(url: String) {
         val request =
-            DownloadManager.Request(Uri.parse(GLIDE_URL))
+            DownloadManager.Request(Uri.parse(url))
                 .setTitle(getString(R.string.app_name))
                 .setDescription(getString(R.string.app_description))
                 .setRequiresCharging(false)
@@ -63,7 +84,7 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private const val GLIDE_URL = "https://github.com/bumptech/glide/archive/master.zip"
-        private const val PROJECT_URL = "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        private const val LOADAPP_URL = "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
         private const val RETROFIT_URL = "https://github.com/square/retrofit/archive/master.zip"
         private const val CHANNEL_ID = "channelId"
     }
